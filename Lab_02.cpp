9e7afc98086fb4249618bfaa77ff8e05e1bbf7a6
misiagi <misiagi@o2.pl>
2013-11-05 21:48:32 +0100

    Create Lab_02.cpp

---
+++ Lab_02.cpp
@@ -0,0 +1,248 @@
+// PoLab2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <time.h>
+#include <sstream>
+#include <istream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+class Student{
+private:
+	string initials;
+	int nr_gr;
+	int marks[10];
+
+public:
+	Student(){};
+	Student(string, int, int*);
+	Student(string);
+	~Student(){};
+	Student(Student&);
+	string toString();
+
+};
+
+class Group{
+private:
+	string name_gr;
+	int l_student;
+	Student *studenci;
+	int indeks;
+
+public:
+	Group(){indeks=0;};
+	Group(string, int);
+	void add(Student);
+	string toString();
+	void file_write(string);
+	void file_read(string);
+	Group(Group&);
+	string File();
+
+};
+
+
+int *tab();
+bool open=1;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	srand ((unsigned int)time(NULL));
+	Student s1("Yogi",210,tab());
+	Student s2("Tama",210,tab());
+	Student s3("Biszkopt",211,tab());
+	Student s4("Piotrek",211,tab());
+	Student s5("Kazi",211,tab());
+
+	Group g1("Grupa1",5);
+	g1.add(s1);
+	g1.add(s2);
+	g1.add(s3);
+	g1.add(s4);
+	g1.add(s5);
+
+	cout<<g1.toString();
+
+	Group g2(g1);
+	cout<<g2.toString();
+
+	g2.file_write("g2.txt");
+
+	Group g3;
+	g3.file_read("g2.txt");
+	cout<<g3.toString();
+	
+	getchar();
+}
+
+//###################################################################################################################################
+
+string Group::File(){
+	time_t t=time(0);
+	stringstream s_plik;
+	struct tm now;
+	localtime_s(&now,&t);
+	s_plik<<"Grupa:"<<name_gr<<"/"
+	<<(now.tm_year+1900)<<'/'
+	<<(now.tm_mon+1)<<'/'<<now.tm_mday
+	<<now.tm_hour<<'/'<<now.tm_min<<'/'
+	<<now.tm_sec<<".txt";
+
+	return(s_plik.str());
+};
+
+void Group::add(Student n_student){
+	if(indeks<l_student) studenci[indeks++]=n_student;
+	else cout<< "Za duzo studentow w grupie";
+};
+
+Group::Group(string _name_gr, int _l_student){
+	name_gr=_name_gr;
+	l_student=_l_student;
+	studenci=new Student[l_student];
+	indeks=0;
+
+};
+
+string Group::toString(){
+	stringstream ss_group;
+	ss_group<<name_gr<<";"<<l_student<<";"<<endl;
+	for(int i=0;i<indeks;i++)
+	{
+		ss_group<<studenci[i].toString();
+	};
+	return(ss_group.str());
+};
+
+void Group::file_read(string file){
+	ifstream inputFile(file);
+	if(!inputFile.is_open())
+		throw std::exception("Brak pliku!");
+
+	string line;
+	inputFile>>line;
+	string bufor[2];
+	int licz=0;
+
+	for(int i=0;i<line.length();i++) {if (line[i]!=';') bufor[licz]+=line[i]; else licz++;}
+	name_gr=bufor[0];
+	istringstream s_l_student(bufor[1]);
+	s_l_student>>l_student;
+
+	//delete [] studenci;
+	studenci = new Student[l_student];
+
+
+	for(int j=0;j<l_student;j++)
+	{
+
+
+		string line="";
+		inputFile>>line;
+
+		
+			Student* tmp=new Student (line);
+			add(*tmp);
+	};
+	inputFile.close();
+};
+
+
+void Group::file_write(string file){
+	//string file = File();
+	ofstream outputFile(file);
+	if (!outputFile.is_open())
+		throw std::exception("Brak pliku!");
+
+	outputFile<<name_gr<<";"<<l_student<<";"<<endl;
+	for(int i=0;i<indeks;i++)
+	{
+		outputFile<<studenci[i].toString();
+	};
+	outputFile.close();
+};
+
+Group::Group(Group& src){
+	
+	name_gr=src.name_gr;
+	l_student=src.l_student;
+	indeks=0;
+	studenci=new Student[l_student];
+
+	for(int i=0;i<l_student;i++)
+	{
+		Student Bufor(src.studenci[i]);
+		add(Bufor);
+	};
+	indeks=src.indeks;
+};
+
+
+//#######################################################################################################################################3
+
+Student::Student(string line){
+
+	string bufor_s[12];
+	int licz=0;
+	for(int i=0;i<line.length();i++) if (line[i]!=';') bufor_s[licz]+=line[i]; else licz++;
+	initials=bufor_s[0];
+	istringstream _nr_gr(bufor_s[1]);
+	_nr_gr>>nr_gr;
+
+		for(int k=2;k<12;k++)
+		{
+			istringstream _marks(bufor_s[k]);
+			_marks>>marks[k-2];
+		};
+
+};
+
+Student::Student(string _initials, int _nr_gr, int *tab){
+	initials=_initials;
+	nr_gr=_nr_gr;
+	for(int i=0;i<10;i++){
+		marks[i]=tab[i];
+	};
+
+}
+
+Student::Student(Student& src){
+	initials=src.initials;
+	nr_gr=src.nr_gr;
+	for(int i=0;i<10;i++)
+	{
+		marks[i]=src.marks[i];
+	};
+}
+
+string Student::toString(){
+
+	stringstream ss_student;
+	ss_student<<initials<<";"<<nr_gr<<";";
+	for(int i=0;i<10;i++)
+	{
+		ss_student<<marks[i]<<";";
+	};
+	ss_student<<endl;
+	return(ss_student.str());
+}
+
+//#######################################################################################################################################333
+
+int* tab(){
+	int *tab=new int[10];
+	for(int i=0;i<10;i++)
+	{
+		tab[i]=rand()%4+2;
+	};
+	return(tab);
+}
+
